a50ba2d42a078ac97a5b98b913941781e44ecd82
Gabriel Fernella <gabrielfernella07@hotmail.com>
2025-04-21 04:40:18 -0300

    feat: add route to modules

---
+++ course/src/main/java/com/ead/course/controllers/ModuleController.java
@@ -0,0 +1,104 @@
+package com.ead.course.controllers;
+
+import com.ead.course.dtos.CourseDTO;
+import com.ead.course.dtos.ModuleDTO;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.ModuleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ModuleController {
+
+    @Autowired
+    ModuleService moduleService;
+
+    @Autowired
+    CourseService courseService;
+
+    @PostMapping("/courses/{courseId}/modules")
+    public ResponseEntity<Object> saveModule(@PathVariable(value = "courseId") UUID courseId , @RequestBody @Valid ModuleDTO moduleDTO) {
+
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+
+        if (!courseModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found.");
+        }
+
+        var moduleModel = new ModuleModel();
+
+        BeanUtils.copyProperties(moduleDTO, moduleModel);
+        moduleModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        moduleModel.setCourse(courseModelOptional.get());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(moduleService.save(moduleModel));
+    }
+
+    @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> saveModule(@PathVariable(value = "courseId") UUID courseId , @PathVariable(value = "moduleId") UUID moduleId) {
+
+        Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
+
+        if (!moduleModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course.");
+        }
+
+        moduleService.delete(moduleModelOptional.get());
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Module deleted successfully.");
+    }
+
+    @PutMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> updateModule(@PathVariable(value = "courseId") UUID courseId,
+                                               @PathVariable(value = "moduleId") UUID moduleId,
+                                               @RequestBody @Valid ModuleDTO moduleDTO) {
+
+        Optional<ModuleModel>module = moduleService.findModuleIntoCourse(courseId, moduleId);
+
+        if (!module.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course.");
+        }
+
+        var moduleUpdated = module.get();
+        moduleUpdated.setTitle(moduleDTO.getTitle());
+        moduleUpdated.setDescription(moduleDTO.getDescription());
+
+        return ResponseEntity.status(HttpStatus.OK).body(moduleService.save(moduleUpdated));
+    }
+
+    @GetMapping("/courses/{courseId}/modules")
+    public ResponseEntity<List<ModuleModel>> getAllModules(@PathVariable(value = "courseId") UUID courseId) {
+
+        var modules = moduleService.findAllByCourse(courseId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(modules);
+    }
+
+    @GetMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> findOneModule(@PathVariable(value = "courseId") UUID courseId, @PathVariable(value = "moduleId") UUID moduleId) {
+
+        Optional<ModuleModel>module = moduleService.findModuleIntoCourse(courseId, moduleId);
+        if (!module.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(module.get());
+    }
+
+
+
+
+}
